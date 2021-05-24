155d25453dff4f203edfadfb4360d9c123b99e06
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-24 21:27:21 +0600

    Creating Navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap'
+import './Header.css'
+import Logo from '../../assets/logo.png'
+
 
 const Header = () => {
  return (
-  <div>
-   <p>Header</p>
+  <div className="Navigation">
+   <Navbar className="Navbar">
+    <div className="container">
+    <NavbarBrand className="Brand" href="/"><img src={Logo} alt="logo"/></NavbarBrand>
+    <Nav>
+     <NavItem>
+      <NavLink className="NavLink" href="#">Something</NavLink>
+     </NavItem>
+     </Nav>
+    </div>
+   </Navbar>
   </div>
  )
 }
