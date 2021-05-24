6cd75d00386e28fedfb836558b24d2ed87c171f4
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-24 20:21:29 +0600

    Project Setup

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BurgerBuilder from './BurgerBuilder.js/BurgerBuilder'
+import Header from './Header/Header'
+
+const Main = () => {
+ return (
+  <div>
+   <Header />
+   <BurgerBuilder/>
+  </div>
+ )
+}
+
+export default Main
