2be09ffc2909a56375a7de470e5d623dcab6965d
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-24 22:44:50 +0600

    Show Ingredients according to Component

--- src/Components/BurgerBuilder.js/Burger/Burger.js
+++ src/Components/BurgerBuilder.js/Burger/Burger.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Ingredient from '../Ingredient/Ingredient'
+import './Burger.css'
 
-const Burger = () => {
+const Burger = props => {
+ let ingredientArr = props.ingredients.map(item => {
+  let amountArr = [...Array(item.amount).keys()];
+  return amountArr.map(_ => {
+   return <Ingredient type={item.type} key={amountArr.length} />
+  })
+ })
+
+  .reduce((arr, element) => {
+   return arr.concat(element)
+  }, []);
+
+ if (ingredientArr.length === 0) {
+ ingredientArr=<p>Please Add Some Ingredient</p>
+}
+ 
  return (
-  <div>
+  <div className="Burger">
    <Ingredient type="bread-top" />
+   {ingredientArr}
    <Ingredient type="bread-bottom" />
   </div>
  )
