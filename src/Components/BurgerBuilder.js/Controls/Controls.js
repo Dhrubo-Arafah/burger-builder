6cdc2eb09e29709ea8247110f71af399e2364e10
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-26 08:00:38 +0600

    Update and Show Price

--- src/Components/BurgerBuilder.js/Controls/Controls.js
+++ src/Components/BurgerBuilder.js/Controls/Controls.js
@@ -3,46 +3,46 @@ import { Card, CardBody, CardFooter, CardHeader } from 'reactstrap'
 import './Controls.css'
 
 const controls = [
- {label:'Salad' , type:'salad'},
- { label: 'Cheese', type: 'cheese'},
- { label: 'Meat', type: 'meat'}
+  { label: 'Salad', type: 'salad' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ]
 
 const BuildControl = props => {
- return (
-  <div className="BuildControl d-flex justify-content-between" >
-   <div className="mr-auto ml-5">{props.label}</div>
-   <div className="btn-group">
-    <button className="btn btn-warning btn-sm m-1" onClick={props.remove}>Less</button>
-    <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
-   </div>
-  </div>
- )
+  return (
+    <div className="BuildControl d-flex justify-content-between" >
+      <div className="mr-auto ml-5">{props.label}</div>
+      <div className="btn-group">
+        <button className="btn btn-warning btn-sm m-1" onClick={props.remove}>Less</button>
+        <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
+      </div>
+    </div>
+  )
 }
 
 const Controls = props => {
- 
- return (
-  <div className="container ml-md-5" style={{textAlign:"center"}}>
-   <Card className="Controls">
-    <CardHeader className="CardHeader"><h4>Add Ingredients</h4></CardHeader>
-    
-    <CardBody>{
-      controls.map(item => {
-       return <BuildControl
-        label={item.label}
-        type={item.type}
-        key={Math.random()}
-        added={() => props.ingredientAdded(item.type)}
-        remove={() => props.removeIngredient(item.type)}
-       />
-      })
-    }
-    </CardBody>
-    <CardFooter><h5>Price: BDT</h5></CardFooter>
-   </Card>
-  </div>
- )
+  console.log(props)
+  return (
+    <div className="container ml-md-5" style={{ textAlign: "center" }}>
+      <Card className="Controls">
+        <CardHeader className="CardHeader"><h4>Add Ingredients</h4></CardHeader>
+
+        <CardBody>{
+          controls.map(item => {
+            return <BuildControl
+              label={item.label}
+              type={item.type}
+              key={Math.random()}
+              added={() => props.ingredientAdded(item.type)}
+              remove={() => props.removeIngredient(item.type)}
+            />
+          })
+        }
+        </CardBody>
+        <CardFooter><h5>Price: {props.price}BDT</h5></CardFooter>
+      </Card>
+    </div>
+  )
 }
 
 export default Controls
